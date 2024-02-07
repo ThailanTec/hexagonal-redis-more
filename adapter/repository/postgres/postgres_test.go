e9efa35c480c48b4e875735ecf6108d180aca9f8
Thailan dos Santos de Jesus <thailan.jesus@engdb.com.br>
2024-02-06 22:12:14 -0300

    first commit
    Signed-off-by: Thailan dos Santos de Jesus <thailan.jesus@engdb.com.br>

--- adapter/repository/postgres/postgres_test.go
+++ adapter/repository/postgres/postgres_test.go
@@ -51,3 +51,47 @@ func TestCreateUserError(t *testing.T) {
 	// Verificar se o método CreateUser foi chamado com os argumentos corretos
 	mockRepo.AssertCalled(t, "CreateUser", expectedUser)
 }
+
+// GET test
+func TestGetUser(t *testing.T) {
+	// Criar um mock do repositório Postgres
+	mockRepo := new(MockPostgres)
+
+	// Configurar comportamento esperado do mock
+	expectedID := 1
+	expectedUser := domain.User{ID: expectedID, Name: "Test User", Email: "thaialn"}
+	mockRepo.On("GetUser", expectedID).Return(expectedUser, nil)
+
+	// Chamar a função GetUser no mock
+	user, err := mockRepo.GetUser(expectedID)
+
+	// Verificar se não houve erro
+	assert.NoError(t, err, "Erro inesperado ao buscar usuário")
+
+	// Verificar se o usuário retornado pelo mock corresponde ao esperado
+	assert.Equal(t, expectedUser, user, "Usuário retornado não corresponde ao esperado")
+
+	// Verificar se o método GetUser foi chamado com os argumentos corretos
+	mockRepo.AssertCalled(t, "GetUser", expectedID)
+}
+
+func TestDeleteUser(t *testing.T) {
+	// Criar um mock do repositório Postgres
+	mockRepo := new(MockPostgres)
+
+	// Configurar comportamento esperado do mock
+	expectedID := 1
+	mockRepo.On("DeleteUser", expectedID).Return(true, nil)
+
+	// Chamar a função DeleteUser no mock
+	deleted, err := mockRepo.DeleteUser(expectedID)
+
+	// Verificar se não houve erro
+	assert.NoError(t, err, "Erro inesperado ao deletar usuário")
+
+	// Verificar se o usuário foi deletado com sucesso
+	assert.True(t, deleted, "Usuário não foi deletado com sucesso")
+
+	// Verificar se o método DeleteUser foi chamado com os argumentos corretos
+	mockRepo.AssertCalled(t, "DeleteUser", expectedID)
+}
